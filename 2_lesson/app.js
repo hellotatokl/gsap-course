2d76323ab126e7a35f3eee65de29d9fc963987b9
hellotatokl <flocus.lol@gmail.com>
2024-03-12 14:00:39 +0300

    asd

--- 2_lesson/app.js
+++ 2_lesson/app.js
@@ -1,14 +1,5 @@
-const titleProject = 'Название проекта';
-const screensValue = 'шаблонные, с уникальным дизайном, с анимациями';
-let screenPrice = 2500;
-let percentage = 50;
-let fullPrice = 1000;
-const responsive = true;
-
-console.log (typeof titleProject)
-console.log (typeof screensValue)
-console.log (typeof screenPrice)
-console.log (typeof percentage)
-console.log (typeof fullPrice)
-console.log (typeof responsive)
-
+let firstName = 'Vlad'
+let ageNum = "23"
+let cityName = "Kazan"
+let hobbieName ="Music"
+console.log('Hellooo', firstName, ageNum, cityName, hobbieName)
