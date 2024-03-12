76ce1e78db15f58a8f1ab6b21870c79aec02c5ea
hellotatokl <flocus.lol@gmail.com>
2024-03-12 11:49:16 +0300

    First commit

---
+++ app.js
@@ -0,0 +1,9 @@
+//const firstName = 'George'
+//firstName = 'Ivan'
+//console.log('hello ', firstName)
+
+let firstName = 'George'
+firstName ='Ivan'
+console.log('Hello', firstName)
+
+var lastName = ''
