50972f7a5bfcc69a968ec51e747db44ad301a997
hellotatokl <flocus.lol@gmail.com>
2024-03-12 13:48:48 +0300

    test

---
+++ 2_lesson/app.js
@@ -0,0 +1,14 @@
+const titleProject = 'Название проекта';
+const screensValue = 'шаблонные, с уникальным дизайном, с анимациями';
+let screenPrice = 2500;
+let percentage = 50;
+let fullPrice = 1000;
+const responsive = true;
+
+console.log (typeof titleProject)
+console.log (typeof screensValue)
+console.log (typeof screenPrice)
+console.log (typeof percentage)
+console.log (typeof fullPrice)
+console.log (typeof responsive)
+
