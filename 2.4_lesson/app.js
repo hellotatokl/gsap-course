64959bdb1bbb767149d826c171a1a9320768c2c6
hellotatokl <flocus.lol@gmail.com>
2024-03-18 13:55:40 +0300

    lesson04 new

--- 2.4_lesson/app.js
+++ 2.4_lesson/app.js
@@ -44,3 +44,59 @@ else if (fullPrice === 50000) {
 else {
     console.log("Что-то пошло не так");
 }
+
+const getAllServicePrices = function() {
+    let allServicePrices = +servicePrice1+ +servicePrice2;
+    console.log ('Результат функции – стоимость всех доп услуг –', allServicePrices)
+    return allServicePrices
+}
+
+const allServicePrices = getAllServicePrices();
+
+function getFullPrice () {
+    let fullPrice = + allServicePrices + +screenPrice;
+    console.log ('Результат 2й функции - Стоимость услуг + верстки –', fullPrice)
+    return fullPrice;
+}
+const FullPrice = getFullPrice();
+
+const getTitle = function (titleProject) {
+    titleProject = titleProject.charAt(0).toUpperCase() + titleProject.slice(1).toLowerCase();
+    return titleProject;
+}
+titleProject = getTitle(titleProject);
+console.log('Результат 3й функции - название проекта', titleProject);
+
+const getServicePercentPrices = function () {
+    let servicePercentPrice = fullPrice - (fullPrice*partnersPercent/100);
+    return servicePercentPrice
+}
+servicePercentPrice = getServicePercentPrices();
+console.log('Результат функции 4 стоимость - проц партнеру', servicePercentPrice);
+
+const getRollbackMessage = function () {
+    servicePercentPrice = Math.round(servicePercentPrice);
+    console.log('Цена с партнерским процентом', servicePercentPrice);
+    if (fullPrice > 50000) { 
+        console.log ('Вам положена скидка 10%');
+    } else if (fullPrice > 20000 && fullPrice < 50000) {
+    console.log ('Вам положена скидка 5%');
+}
+else if (fullPrice > 0 && fullPrice < 20000) {
+    console.log ('Скидка не предусмотрена');
+}
+else if (fullPrice === 0) {
+    console.log("Просто текст");
+}
+else if (fullPrice === 20000) {
+    console.log("Просто текст");
+}
+else if (fullPrice === 50000) {
+    console.log("Просто текст");
+}
+else {
+    console.log("Что-то пошло не так");
+}
+
+}
+getRollbackMessage ()
